998a3eeee85501418c4ee1f881fa6740378db96c
bledda <bledda@student.42nice.fr>
2022-11-22 19:49:54 +0100

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,113 @@
+<script lang="ts">
+	import Button from "./Button.svelte";
+</script>
+
+<div class="right">
+	<h1>Buttons</h1>
+
+	<section>
+		<div class="element">
+			<p>{"<Button />"}</p>
+			<Button />
+		</div>
+		<div class="element">
+			<p>&:hover, &:focus</p>
+			<Button />
+		</div>
+	</section>
+
+	<section>
+		<div class="element">
+			<p>{"<Button variant=\"outline\" />"}</p>
+			<Button variant="outline" />
+		</div>
+		<div class="element">
+			<p>&:hover, &:focus</p>
+			<Button variant="outline" />
+		</div>
+	</section>
+
+	<section>
+		<Button variant="text" />
+		<Button variant="text" />
+	</section>
+
+	<section>
+		<Button disableShadow />
+	</section>
+
+	<section>
+		<Button disabled />
+		<Button variant="text" disabled />
+	</section>
+
+	<section>
+		<Button startIcon="local_grocery_store" />
+		<Button endIcon="local_grocery_store" />
+	</section>
+
+	<section>
+		<Button size="sm" />
+		<Button size="md" />
+		<Button size="lg" />
+	</section>
+
+	<section>
+		<Button color="default" />
+		<Button color="primary" />
+		<Button color="secondary" value="Secondary" />
+		<Button color="danger" value="Danger" />
+	</section>
+
+	<section>
+		<Button color="default" />
+		<Button color="primary" />
+		<Button color="secondary" value="Secondary" />
+		<Button color="danger" value="Danger" />
+	</section>
+</div>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Ubuntu Mono:wght@400&display=swap');
+
+	* {
+		margin: 0;
+		padding: 0;
+	}
+
+	h1 {
+		font-family: 'Poppins';
+		font-style: normal;
+		font-weight: 500;
+		font-size: 24px;
+		line-height: 36px;
+		color: #4F4F4F;
+	}
+
+	section {
+		display: flex;
+		gap: 10%;
+		align-items: center;
+	}
+
+	.right {
+		display: flex;
+		flex-direction: column;
+		gap: 33px;
+	}
+
+	.element {
+		min-width: 190px;
+	}
+
+	.element p {
+		font-family: 'Ubuntu Mono';
+		font-style: normal;
+		font-weight: 400;
+		font-size: 12px;
+		line-height: 12px;
+		color: #333333;
+		margin-bottom: 8px;
+	}
+</style>
---
+++ src/routes/Button.svelte
@@ -0,0 +1,159 @@
+<script lang="ts">
+	export let value: string = "Default";
+	export let disabled: boolean = false;
+	export let variant: string = "";
+	export let size: string = "";
+	export let disableShadow: boolean = false;
+	export let color: string = "default";
+	export let startIcon: string = "";
+	export let endIcon: string = "";
+</script>
+
+<button
+	class:text={variant === "text"}
+	class:outline={variant === "outline"}
+	class:sm={size === "sm"}
+	class:md={size === "md" || size === ""}
+	class:lg={size === "lg"}
+	class:disableShadow={disableShadow || disabled || variant != "text"}
+	class:defaultColor={variant === ""
+						&& (color === "default"
+							|| (color != "primary" && color != "secondary"))}
+	class:primaryColor={color === "primary"
+						|| disableShadow
+						|| size != ""
+						|| startIcon != ""
+						|| endIcon != ""}
+	class:secondaryColor={color === "secondary"}
+	class:dangerColor={color === "danger"}
+	class:disable={disabled}
+
+	disabled='{disabled}'
+>
+	{#if startIcon != ""}
+		<span class="material-icons paddingRight">
+			{startIcon}
+		</span>
+	{/if}
+			{value}
+	{#if endIcon != ""}
+		<span class="material-icons paddingLeft">
+			{endIcon}
+		</span>
+	{/if}
+</button>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Noto Sans JP:wght@500&display=swap');
+
+	.material-icons {
+		font-size: 14px;
+		line-height: 18px;
+	}
+
+	.paddingLeft {
+		padding-left: 8px;
+	}
+
+	.paddingRight {
+		padding-right: 8px;
+	}
+
+	button {
+		font-family: 'Noto Sans JP';
+		font-style: normal;
+		font-weight: 500;
+		font-size: 14px;
+		line-height: 20px;
+		text-align: center;
+		box-shadow: 0px 2px 3px rgba(51, 51, 51, 0.2);
+		border-radius: 6px;
+		border: none;
+		cursor: pointer;
+	}
+
+	button:hover, button:focus {
+		box-shadow: 0px 2px 3px rgba(51, 51, 51, 0.2);
+		border-radius: 6px;
+	}
+
+	.disableShadow, .disableShadow:hover, .disableShadow:focus {
+		box-shadow: none;
+	}
+
+	.outline {
+		color: #3D5AFE;
+		border: 1px solid #3D5AFE;
+		background: white;
+	}
+
+	.outline:hover, .outline:focus {
+		background: rgba(41, 98, 255, 0.1);
+	}
+
+	.text {
+		padding: 0;
+		background: transparent !important;
+		box-shadow: none;
+		color: #3D5AFE;
+	}
+
+	.text:hover, .text:focus {
+		background: rgba(41, 98, 255, 0.1) !important;
+		border-radius: 6px;
+		box-shadow: none;
+	}
+
+	.sm {
+		padding: 4px 9px;
+	}
+
+	.md {
+		padding: 8px 18px;
+	}
+
+	.lg {
+		padding: 10px 22px;
+	}
+
+	.defaultColor {
+		color: #3F3F3F;
+		background: #E0E0E0;
+	}
+
+	.defaultColor:hover, .defaultColor:focus {
+		background: #AEAEAE;
+	}
+
+	.primaryColor {
+		color: white;
+		background: #2962FF;
+	}
+
+	.primaryColor:hover, .primaryColor:focus {
+		background: #0039CB;
+	}
+
+	.secondaryColor {
+		color: white;
+		background: #455A64;
+	}
+
+	.secondaryColor:hover, .secondaryColor:focus {
+		background: #1C313A;
+	}
+
+	.dangerColor {
+		color: white;
+		background: #D32F2F;;
+	}
+
+	.dangerColor:hover, .dangerColor:focus {
+		background: #9A0007;
+	}
+
+	.disable {
+		color: #9E9E9E;
+		background: #E0E0E0;
+	}
+</style>
